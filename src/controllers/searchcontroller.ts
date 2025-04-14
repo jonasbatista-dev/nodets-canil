00c31bd9d4eb8fd69049da5741fb6b2e1d51129a
jonas.batista <jonas.batista@spotmetrics.com>
2025-04-14 17:16:51 -0300

    Search pages are working

--- src/controllers/searchcontroller.ts
+++ src/controllers/searchcontroller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
+import { listPets } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 const search = (req: Request, res: Response) => {
-  res.render('pages/search');
+  const { value } = req.query;
+  const list = listPets.getFromName(value as string);
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+  });
 };
 
 export { search };
